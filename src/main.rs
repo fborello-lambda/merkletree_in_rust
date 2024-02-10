587df322acc564df38a09c3f06c6398dc03687de
fborello-lambda <federicoborello@lambdaclass.com>
2024-02-10 18:33:32 -0300

    Change README

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod merkle_tree_vec;
 use crate::merkle_tree::MerkleTreeDs;
 use crate::merkle_tree_vec::MerkleTreeVec;
 fn main() {
-    let mtree = MerkleTreeDs::new(vec!["D".to_string(), "F".to_string(), "E".to_string()]);
-    let mtree_vec = MerkleTreeVec::new(vec!["D".to_string(), "F".to_string(), "E".to_string()]);
+    let mtree = MerkleTreeDs::new(vec!["D".to_string()]);
+    let mtree_vec = MerkleTreeVec::new(vec!["D".to_string()]);
     println!("{mtree}");
     println!("{}", mtree_vec.root);
 }
